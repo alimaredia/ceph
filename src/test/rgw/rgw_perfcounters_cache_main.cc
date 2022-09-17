c155a4d1b9c20eaa0c8147850f350a2354463358
Ali Maredia <amaredia@redhat.com>
2022-09-17 19:31:36 -0400

    rgw: adding perf_counters_cache_key into test program
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/test/rgw/rgw_perfcounters_cache_main.cc
+++ src/test/rgw/rgw_perfcounters_cache_main.cc
@@ -5,6 +5,7 @@
 #include "common/intrusive_lru.h"
 #include "common/perf_counters.h"
 #include "common/ceph_context.h"
+#include "common/perf_counters_cache_key.h"
 
 enum RGWCounters {
   l_rgw_metrics_first = 15000,
@@ -129,39 +130,39 @@ int main() {
   size_t target_size = 1;
   PCountersCache cache(cct, target_size);
 
-  std::string key1 = "sally";
+  std::string key1 = ceph::perf_counters::cache_key("rgw", {{"Bucket","Foo"}, {"User", "Sally"}});
   auto [ref, key_existed] = cache.add(key1);
   std::cout << "instance_labels for entry 1: " << ref->instance_labels << std::endl;
 
 
   uint64_t ret = cache.get_counter(key1, l_rgw_put_b);
-  std::cout << "sally initial l_rgw_put_b is " << ret << std::endl;
+  std::cout << "initial l_rgw_put_b is " << ret << std::endl;
   cache.inc(key1, l_rgw_put_b, 500);
   ret = cache.get_counter(key1, l_rgw_put_b);
-  std::cout << "sally l_rgw_put_b after inc 500 is " << ret << std::endl;
+  std::cout << ref->instance_labels << " l_rgw_put_b after inc 500 is " << ret << std::endl;
   cache.set_counter(key1, l_rgw_put_b, 300);
   ret = cache.get_counter(key1, l_rgw_put_b);
-  std::cout << "sally l_rgw_put_b after set 300 is " << ret << std::endl;
+  std::cout << ref->instance_labels << " l_rgw_put_b after set 300 is " << ret << std::endl;
   cache.dec(key1, l_rgw_put_b, 50);
   ret = cache.get_counter(key1, l_rgw_put_b);
-  std::cout << "sally l_rgw_put_b after dec 50 is " << ret << std::endl;
+  std::cout << ref->instance_labels << " l_rgw_put_b after dec 50 is " << ret << std::endl;
   std::cout << std::endl;
 
-  std::string key2 = "harry";
+  std::string key2 = ceph::perf_counters::cache_key("rgw", {{"Bucket","Foo"}, {"User", "Harry"}});
   auto [ref2, key_existed2] = cache.add(key2);
   std::cout << "instance_labels for entry 2: " << ref2->instance_labels << std::endl;
 
   ret = cache.get_counter(key2, l_rgw_get_b);
-  std::cout << "harry initial l_rgw_get_b is " << ret << std::endl;
+  std::cout << ref2->instance_labels << " initial l_rgw_get_b is " << ret << std::endl;
   cache.set_counter(key2, l_rgw_get_b, 400);
   ret = cache.get_counter(key2, l_rgw_get_b);
-  std::cout << "harry l_rgw_get_b after set 400 is " << ret << std::endl;
+  std::cout << ref2->instance_labels << " l_rgw_get_b after set 400 is " << ret << std::endl;
   cache.inc(key2, l_rgw_get_b, 1000);
   ret = cache.get_counter(key2, l_rgw_get_b);
-  std::cout << "harry l_rgw_get_b after inc 1000 is " << ret << std::endl;
+  std::cout << ref2->instance_labels << " l_rgw_get_b after inc 1000 is " << ret << std::endl;
   cache.dec(key2, l_rgw_get_b, 500);
   ret = cache.get_counter(key2, l_rgw_get_b);
-  std::cout << "harry l_rgw_get_b after dec 500 is " << ret << std::endl;
+  std::cout << ref2->instance_labels << " l_rgw_get_b after dec 500 is " << ret << std::endl;
 
   delete cct;
 
