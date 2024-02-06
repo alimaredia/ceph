0767bd4fcb18d22a7c142060dc6a54b971fb84c6
Ali Maredia <amaredia@redhat.com>
2024-02-06 13:57:31 -0500

    qa: try to fix url_file string
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- qa/tasks/rgw.py
+++ qa/tasks/rgw.py
@@ -447,8 +447,9 @@ def task(ctx, config):
     for client in clients:
         url = ctx.rgw.role_endpoints[client].url()
         testdir = teuthology.get_testdir(ctx)
-        url_file = '{tdir}/url_file'.format(tdir=testdir),
-        ctx.cluster.only(client).run(args=['sudo', 'echo', '-n', '{url}'.format(url=url), run.Raw('|'), 'sudo', 'tee', '{file}'.format(file=url_file)])
+        url_file = '{tdir}/url_file'.format(tdir=testdir)
+        log.info("url file path is: {file}".format(file=url_file))
+        ctx.cluster.only(client).run(args=['sudo', 'echo', '-n', '{url}'.format(url=url), run.Raw('|'), 'sudo', 'tee', url_file])
         ctx.cluster.only(client).run(args=['sudo', 'chown', 'ceph', url_file])
         log.info("url file content")
         ctx.cluster.only(client).run(args=['cat', url_file])
